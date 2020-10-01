c8e0098bf194969bc653cb3086c82a3e40c6f04b
Andrés del Cid <adelcidc2@gmail.com>
2020-09-30 22:49:49 -0600

    keep it in

--- variables.php
+++ variables.php
@@ -11,8 +11,24 @@ echo 'Mi nombre es: '.$nombre;
 echo '<br><br>'; 
 
 
+//booleana
 
+$booleana = true;
 
+echo 'variable booleana: '.$booleana;
+echo '<br><br>'; 
 
+//variable tipo arreglo
+
+$arreglo = array("Andres", "Andrea");
+
+echo $arreglo[1];
+echo '<br><br>'; 
+
+//variable objeto (imporatant)
+$objeto = (object)["mueble1"=>"armario", "mueble2"=>"escritorio"];
+
+echo $objeto->mueble2;
+echo '<br><br>'; 
 
 ?>
